81056a172479b23e15845041c2ecd6eef148549c
Samuel Mascarell <Samuel@MacBook-de-Samuel.local>
2013-05-19 12:00:23 +0200

    TestView Add

---
+++ Notikumi Checkpoint/TestView.h
@@ -0,0 +1,13 @@
+//
+//  TestView.h
+//  Notikumi Checkpoint
+//
+//  Created by Samuel Mascarell on 19/05/13.
+//  Copyright (c) 2013 Samuel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestView : UIView
+
+@end
